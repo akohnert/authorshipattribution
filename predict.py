ba757623a630cce5c2f493ef50893d74d837084d
annalena <annalenakohnert@gmail.com>
2020-08-27 23:50:43 +0200

    Add prediction class

---
+++ predict.py
@@ -0,0 +1,46 @@
+from tweet_features import TweetFeatures
+from data_set_features import DataSetFeatures
+import pandas as pd
+from collections import Counter
+
+class PredictAuthor:
+
+    def __init__(self):
+        self.test_cases = pd.DataFrame()
+        self.predictions = []
+
+    def best_match(self, test_file, gold_file):
+        predictions = []
+        gold = pd.read_csv(gold_file)
+        test_cases = DataSetFeatures(test_file).extract_features()
+        for i in test_cases.index:
+            D = Counter()
+            for feature in test_cases:
+                if feature not in ['author', 'tweet_id']:
+                    for j in gold.index:
+                        D[gold['author'][j]] += abs(test_cases[feature][i]-gold[feature][j])
+            predictions.append(min(D, key=D.get))
+        assert(len(predictions) == test_cases.shape[0])
+        self.test_cases = test_cases
+        self.predictions  = predictions
+        return test_cases, predictions
+
+    def save_predictions(self, filename):
+        if self.test_cases.empty:
+            raise Exception()
+        self.test_cases.insert(loc=1, column='predicted_author', value=self.predictions)
+        self.test_cases.to_csv(filename)
+
+    def evaluate(self):
+        if self.test_cases.empty:
+            raise Exception()
+        matches = Counter()
+        for author in self.test_cases.author.unique():
+            tweets_by_author = self.test_cases.index[self.test_cases.author == author].tolist()
+            for i in tweets_by_author:
+                if self.test_cases.author[i] == self.test_cases.predicted_author[i]:
+                    matches[author] += 1
+                    matches['total'] += 1
+            matches[author] = matches[author] / len(tweets_by_author)
+        matches['total'] = matches['total'] / self.test_cases.shape[0]
+        return matches
