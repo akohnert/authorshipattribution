72aa375c1557b6394bbe38765bf5b98d2b84f6e0
annalena <annalenakohnert@gmail.com>
2020-08-27 13:04:44 +0200

    Add class to extract and save features for entire dataset

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pandas as pd
 from extract_features import FeatureExtraction
+from data_set_features import DataSetFeatures
 
 def main():
-    data = pd.read_csv('../dev_set.csv')
-    for i, tweet in data.iterrows():
-        output = FeatureExtraction(tweet).all_features()
-        print(output)
+    f = DataSetFeaturesFeatures('../dev_set.csv')
+    f.save_features('dev_set_features.csv')
+    f.aggregate_features('dev_set_features_aggregated.csv')
+    #data = pd.read_csv('../dev_set.csv')
+    #for i, tweet in data.iterrows():
+    #    output = FeatureExtraction(tweet).all_features()
+    #    print(output)
 
 
 
